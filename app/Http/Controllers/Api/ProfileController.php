63913b6d80f43d648e75a379bbf3b76a994802dd
vicky <pratamavr@gmail.com>
2026-05-14 01:56:08 +0800

    updates

--- app/Http/Controllers/Api/ProfileController.php
+++ app/Http/Controllers/Api/ProfileController.php
@@ -15,7 +15,7 @@ class ProfileController extends Controller
     $user = auth()->user();
 
     return response()->json([
-      'id' => $user->id,
+      'id' => $user->telegram_id ?? $user->id,
       'first_name' => $user->first_name,
       'last_name' => $user->last_name,
       'username' => $user->username ?? null,
