5bc47a51ace6e410f84546877355367537ec7bb8
vicky <pratamavr@gmail.com>
2026-05-14 01:13:29 +0800

    updates

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 use Modules\Notes\Http\Controllers\NotesController;
 
 Route::middleware(['auth:sanctum'])->prefix('apps')->name('apps.')->group(function () {
-  Route::get('notes/{any?}', [NotesController::class, 'index'])->where('any', '.*');
+  Route::get('notes', [NotesController::class, 'index']);
 });
