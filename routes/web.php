df8eaf56e6e17ab04c287d4d1756a6e22a976aec
vicky <pratamavr@gmail.com>
2026-05-31 11:33:40 +0800

    updates

--- routes/web.php
+++ routes/web.php
@@ -9,33 +9,46 @@ use Modules\Notes\Http\Controllers\Web\ProfileController;
 use Modules\Notes\Http\Controllers\Web\TrashController;
 
 Route::middleware(['web', 'auth'])->prefix('notes')->name('notes.web.')->group(function () {
-  // Home / Dashboard
+  // Halaman khusus (spesifik) – letakkan di atas
   Route::get('/home', [NoteController::class, 'home'])->name('home');
-
-  // Catatan CRUD
-  Route::get('/', [NoteController::class, 'index'])->name('index');
   Route::get('/create', [NoteController::class, 'create'])->name('create');
   Route::post('/', [NoteController::class, 'store'])->name('store');
-  Route::get('/{id}', [NoteController::class, 'show'])->name('show');
-  Route::get('/{id}/edit', [NoteController::class, 'edit'])->name('edit');
-  Route::put('/{id}', [NoteController::class, 'update'])->name('update');
-  Route::delete('/{id}', [NoteController::class, 'destroy'])->name('destroy');
 
-  // Daily
   Route::get('/daily', [DailyController::class, 'index'])->name('daily');
-
-  // Reminders
   Route::get('/reminders', [ReminderController::class, 'index'])->name('reminders');
-  Route::patch('/reminders/{id}/complete', [ReminderController::class, 'complete'])->name('reminders.complete');
-  Route::delete('/reminders/{id}', [ReminderController::class, 'destroy'])->name('reminders.destroy');
-
-  // Profile
   Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
-
-  // Trash
   Route::get('/trash', [TrashController::class, 'index'])->name('trash');
-  Route::patch('/trash/{id}/restore', [TrashController::class, 'restore'])->name('trash.restore');
-  Route::delete('/trash/{id}/force', [TrashController::class, 'forceDelete'])->name('trash.force-delete');
+
+  // Rute generik dengan parameter id – setelah semua rute spesifik
+  Route::get('/', [NoteController::class, 'index'])->name('index');
+  Route::get('/{id}', [NoteController::class, 'show'])
+  ->where('id', '[0-9]+')
+  ->name('show');
+  Route::get('/{id}/edit', [NoteController::class, 'edit'])
+  ->where('id', '[0-9]+')
+  ->name('edit');
+  Route::put('/{id}', [NoteController::class, 'update'])
+  ->where('id', '[0-9]+')
+  ->name('update');
+  Route::delete('/{id}', [NoteController::class, 'destroy'])
+  ->where('id', '[0-9]+')
+  ->name('destroy');
+
+  // Reminder actions
+  Route::patch('/reminders/{id}/complete', [ReminderController::class, 'complete'])
+  ->where('id', '[0-9]+')
+  ->name('reminders.complete');
+  Route::delete('/reminders/{id}', [ReminderController::class, 'destroy'])
+  ->where('id', '[0-9]+')
+  ->name('reminders.destroy');
+
+  // Trash actions
+  Route::patch('/trash/{id}/restore', [TrashController::class, 'restore'])
+  ->where('id', '[0-9]+')
+  ->name('trash.restore');
+  Route::delete('/trash/{id}/force', [TrashController::class, 'forceDelete'])
+  ->where('id', '[0-9]+')
+  ->name('trash.force-delete');
 });
 
 Route::prefix('apps')->name('apps.')->group(function () {
